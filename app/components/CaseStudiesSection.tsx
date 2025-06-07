3988f7eb5f1ab1aa06eb7259b77b88c05e6df05d
jo <jyothishkpk@gmail.com>
2025-06-07 20:54:30 +0530

    yrtyt

--- app/components/CaseStudiesSection.tsx
+++ app/components/CaseStudiesSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef} from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "slick-carousel/slick/slick.css";
